c26307b0b5c3380b81508931c5372563c6459bae
rh-robin <rhrobin8880@gmail.com>
2025-02-25 14:19:37 +0600

    login bug fix

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -16,7 +16,7 @@ class AuthenticatedSessionController extends Controller
      */
     public function create(): View
     {
-        return view('auth.login2');
+        return view('auth.login');
     }
 
     /**
